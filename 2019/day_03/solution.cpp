0a5a478a57d5d7238653c20e335096047ae4c734
James Cryne <jpc666@cs.bham.ac.uk>
2020-12-29 12:04:45 +0000

    Day 3 P2 complete

--- 2019/day_03/solution.cpp
+++ 2019/day_03/solution.cpp
@@ -2,7 +2,7 @@
 #include<fstream>
 #include<vector>
 #include<unordered_set>
-
+#include<unordered_map>
 // Only for pairs of std::hash-able types for simplicity.
 // You can of course template this struct to allow other hash functions
 struct pair_hash {
@@ -32,33 +32,29 @@ std::vector<std::string> store_instruction(std::string s) {
   return v;
 }
 
-std::unordered_set<std::pair<int,int>, pair_hash> parse_instruction(std::vector<std::string> instructions) {
+std::unordered_set<std::pair<int,int>, pair_hash> parse_instruction(std::vector<std::string> instructions, std::unordered_map<std::pair<int,int>, int, pair_hash> &m) {
   std::unordered_set<std::pair<int,int>, pair_hash> result;
   std::pair<int,int> loc = std::make_pair(0,0);
-
+  int steps{0};
   for(auto instruction : instructions) {
-    std::cout << "Instruction: " << instruction << std::endl;
     int dest = stoi(instruction.substr(1, instruction.size()-1));
-    if(instruction.at(0) == 'U' || instruction.at(0) == 'D') {
-      int start =  (instruction.at(0) == 'U') ? loc.second+1 : loc.second-1;
-      dest = (instruction.at(0) == 'U') ? dest + start : start - dest;
-      loc.second = (instruction.at(0) == 'U') ? dest-1 : dest+1;
-      if(start > dest) std::swap(start, dest);
-      //std::cout << "Start: " << start << ", Dest: " << dest << std::endl;
-      for(int i=start; i<dest;i++) {
-        result.insert(std::make_pair(loc.first, i));
+    char dir = instruction.at(0);
+    int i{loc.first},j{loc.second};
+    for(int k=0;k<dest;k++) {
+      switch(dir) {
+        case 'U' : i++;
+            break;
+        case 'D' : i--;
+            break;
+        case 'R' : j++;
+            break;
+        case 'L' : j--;
+            break;
       }
-      std::cout << "Line Drawn to: (" << loc.first << ", " << loc.second << ")\n";
-    } else if(instruction.at(0) == 'R' || instruction.at(0) == 'L') {
-      int start = (instruction.at(0) == 'R') ? loc.first+1 : loc.first-1;
-      dest = (instruction.at(0) == 'R') ? dest + start : start - dest;
-      loc.first = (instruction.at(0) == 'R') ? dest-1 : dest+1;
-      if(start > dest) std::swap(start, dest);
-      //std::cout << "Start: " << start << ", Dest: " << dest << std::endl;
-      for(int i=start; i<dest;i++) {
-        result.insert(std::make_pair(i, loc.second));
-      }
-      std::cout << "Line Drawn to: (" << loc.first << ", " << loc.second << ")\n";
+      steps++;
+      result.insert(std::make_pair(i, j));
+      loc.first=i;loc.second=j;
+      m.insert({std::make_pair(i,j), steps});
     }
   }
   return result;
@@ -71,11 +67,14 @@ int main(void) {
   //First Line
   getline(input, line);
   std::vector<std::string> path_1 = store_instruction(line);
-  std::unordered_set<std::pair<int,int>, pair_hash> points_1 = parse_instruction(path_1);
+  std::unordered_map<std::pair<int,int>,int, pair_hash> steps_taken_1;
+  std::unordered_set<std::pair<int,int>, pair_hash> points_1 = parse_instruction(path_1, steps_taken_1);
+
   //Second Line
   getline(input, line);
   std::vector<std::string> path_2 = store_instruction(line);
-  std::unordered_set<std::pair<int,int>, pair_hash> points_2 = parse_instruction(path_2);
+  std::unordered_map<std::pair<int,int>,int, pair_hash> steps_taken_2;
+  std::unordered_set<std::pair<int,int>, pair_hash> points_2 = parse_instruction(path_2, steps_taken_2);
 
   int p1{100000};
   for(auto elem : points_2) {
@@ -85,9 +84,16 @@ int main(void) {
       //std::cout << "Intersection Found: (" << elem.first << ", " << elem.second << ")\n";
     }
   }
-
-
   std::cout << "Part 1: " << p1 << std::endl;
+
+  int p2{100000};
+  for(auto elem : points_2) {
+    if(points_1.find(elem) != points_1.end()) {
+      int dist = steps_taken_1.find(elem) -> second + steps_taken_2.find(elem) -> second;
+      p2 = (p2 > dist) ? dist : p2;
+    }
+  }
+  std::cout << "Part 2: " << p2 << std::endl;
 
   return 0;
 }
